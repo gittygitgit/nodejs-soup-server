b483f81bb19002361eb11f573920ad072c6076b9
Mike Grudkowski <grudkowm@michaels-air-2.attlocal.net>
2015-12-31 17:22:36 -0800

    Add sample test

---
+++ test/test1.js
@@ -0,0 +1,10 @@
+var expect    = require("chai").expect;
+
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+});
+
